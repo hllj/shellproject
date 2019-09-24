619958243506bb032eb6d5fcea71d17473d40bc5
hllj <vanhop3499@gmail.com>
2019-09-24 15:33:09 +0700

    Finish function 3 and 4

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 
 #define MAXLINE 80 /* The maximum length command */
 
-void parse(int *nparameter, int *concurrent, char *line, char **argv)
+void parse(int *nparameter, int *concurrent, char *line, char **argv, int *pos)
 {
     *nparameter = 0;
     while (*line != '\0') {       
@@ -11,16 +11,20 @@ void parse(int *nparameter, int *concurrent, char *line, char **argv)
             *line++ = '\0';     
         if (strcmp(line, "&") == 0) *concurrent = 1;
         else {
+            if (*line == '|' || *line == '>' || *line == '<') {
+                *pos = *nparameter;
+            } 
             *argv++ = line;   
-            (*nparameter)++;       
+            (*nparameter)++;  
         }
+        
         while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n') 
             line++;             
      }
     *argv = '\0';             
 }
 
-void execute(int *nparameter, int *concurrent, char **argv)
+void execute(int *nparameter, int *concurrent, char **argv, int pos)
 {
     pid_t pid;
     int status;
@@ -30,9 +34,112 @@ void execute(int *nparameter, int *concurrent, char **argv)
        exit(1);
     }
     else if (pid == 0) {   
-        if (execvp(*argv, argv) < 0) {     
-            printf("*** ERROR: exec failed\n");
-            exit(1);
+        if (pos == -1) {
+            if (execvp(*argv, argv) < 0) {     
+                printf("*** ERROR: exec failed\n");
+                exit(1);
+            }
+        }
+        else {
+            if (strcmp(argv[pos],">") == 0)
+            {
+                char *new_argv[MAXLINE/2+2];
+                for (int i = 0; i<pos ;i++)
+                {
+                    new_argv[i] = malloc(strlen(argv[i]) + 1);
+                    strcpy(new_argv[i],argv[i]);
+                }
+                new_argv[pos] = '\0';
+                int fd = open(argv[pos+1] , O_WRONLY | O_CREAT | O_TRUNC);
+                dup2(fd,1);
+                close(fd);
+                if (execvp(*new_argv, new_argv) < 0) {     
+                    printf("*** ERROR: exec failed\n");
+                    for (int i = 0; i<pos ;i++)
+                    {
+                        free(new_argv[i]);
+                    }
+                    exit(1);
+                }
+                for (int i = 0; i<pos ;i++)
+                {
+                    free(new_argv[i]);
+                }
+            }else if (strcmp(argv[pos],"<") == 0)
+            {
+                char *new_argv[MAXLINE/2+2];
+                for (int i = 0; i<pos ;i++)
+                {
+                    new_argv[i] = malloc(strlen(argv[i]) + 1);
+                    strcpy(new_argv[i],argv[i]);
+                }
+                new_argv[pos] = '\0';
+                int fd = open(argv[pos+1] , O_RDONLY);
+                dup2(fd,0);
+                close(fd);
+                if (execvp(*new_argv, new_argv) < 0) {     
+                    printf("*** ERROR: exec failed\n");
+                    for (int i = 0; i<pos ;i++)
+                    {
+                        free(new_argv[i]);
+                    }
+                    exit(1);
+                }
+                for (int i = 0; i<pos ;i++)
+                {
+                    free(new_argv[i]);
+                }
+            }else if (strcmp(argv[pos],"|") == 0)
+            {
+                int pfd[2];
+                pipe(pfd);
+                int cpid;
+                cpid = fork();
+                if (cpid == 0)
+                {
+                    close(pfd[0]);
+                    dup2(pfd[1],1);
+                    char *new_argv[MAXLINE/2+2];
+                    for (int i = 0; i<pos ;i++)
+                    {
+                        new_argv[i] = malloc(strlen(argv[i]) + 1);
+                        strcpy(new_argv[i],argv[i]);
+                    }
+                    new_argv[pos] = '\0';
+                    if (execvp(*new_argv, new_argv) < 0) {     
+                    printf("*** ERROR: exec failed\n");
+                    for (int i = 0; i<pos ;i++)
+                    {
+                        free(new_argv[i]);
+                    }
+                    exit(1);
+                    }   
+                    
+                }
+                else if (cpid > 0)
+                {   
+                    char *new_argv[MAXLINE/2+2];
+                    int stt;
+                    wait(&stt);
+                    close(pfd[1]);
+                    dup2(pfd[0],0);
+                    for (int i = pos +1; i<*nparameter ;i++)
+                    {
+                        new_argv[i-pos-1] = malloc(strlen(argv[i]) + 1);
+                        strcpy(new_argv[i-pos-1],argv[i]);
+                    }
+                    new_argv[*nparameter- pos-1] = '\0';
+                    if (execvp(*new_argv, new_argv) < 0) {     
+                    printf("*** ERROR: exec failed1\n");
+                    for (int i = 0; i<*nparameter- pos-1;i++)
+                    {
+                        free(new_argv[i]);
+                    }
+                    exit(1);
+                    }   
+                }
+                
+            }
         }
     }
     else {               
@@ -85,19 +192,21 @@ void saveHistoryBuff(char *historybuff, char *line) {
 void executeHistoryBuff(char *historybuff) {
     int nparameter;
     int concurrent;
+    int pos;
     char  *argv[MAXLINE / 2 + 1];
     if (strcmp(historybuff, "NULL") == 0) {
         printf("No commands in history.");
         return;
     }
-    parse(&nparameter, &concurrent, historybuff, argv);
-    execute(&nparameter, &concurrent, argv);
+    parse(&nparameter, &concurrent, historybuff, argv, &pos);
+    execute(&nparameter, &concurrent, argv, pos);
     return; 
 }
 
 void runshell() {
     int nparameter;
     int concurrent;
+    int pos;
     char  line[MAXLINE];            // Line of command
     char  buffline[MAXLINE];        // Save a line
     char  *argv[MAXLINE / 2 + 1];   // argument list
@@ -106,20 +215,30 @@ void runshell() {
     int run = 1;
     while (run) {
         concurrent = 0;
+        pos = -1;
         printf("osh>");
         gets(line);                 // Read a line of command              
         trim(line, NULL);
         strcpy(buffline, line);     // Save another line command before parsed into argv
         printf("\n");       
         // Parse into a list of arguments delimited by space     
-        parse(&nparameter, &concurrent, line, argv);
+        parse(&nparameter, &concurrent, line, argv, &pos);
+        // print_arguments(&nparameter, &concurrent, line, argv, &pos);
+        // exit(0);
         if (strcmp(argv[0], "!!") == 0) {
             executeHistoryBuff(historybuff);
             continue;
         }          
         if (strcmp(argv[0], "exit") == 0) //Hit exit
             exit(0);            
-        execute(&nparameter, &concurrent, argv);              // Execute an command
+        execute(&nparameter, &concurrent, argv, pos);              // Execute an command
         saveHistoryBuff(historybuff, buffline);
     }
+}
+
+void print_arguments(int *nparameter, int *concurrent, char *line, char **argv, int *pos) {
+    for (int i = 0; i < *nparameter; i++) {
+        printf("%s ", argv[i]);
+    }
+    printf("Position : %d", *pos);
 }
--- functions.h
+++ functions.h
@@ -2,9 +2,13 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
-void parse(int *nparameter, int *concurrent, char *line, char **argv);
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<unistd.h>
 
-void execute(int *nparamter, int *concurrent, char **argv);
+void parse(int *nparameter, int *concurrent, char *line, char **argv, int *pos);
+
+void execute(int *nparamter, int *concurrent, char **argv, int pos);
 
 void saveHistoryBuff(char *historybuff, char* line);
 
@@ -16,3 +20,5 @@ void runshell();
 char *ltrim(char *str, const char *seps);
 char *rtrim(char *str, const char *seps);
 char *trim(char *str, const char *seps); 
+
+void print_arguments(int *nparameter, int *concurrent, char *line, char **argv, int *pos);
